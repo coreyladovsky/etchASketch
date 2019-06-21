7adb2d7cfd757ce28555ca90af5f865f805d038b
Corey Ladovsky <coreyskymusic@gmail.com>
2019-06-21 12:00:06 -0400

    smoother movement

--- index.js
+++ index.js
@@ -1,61 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#reset").addEventListener("click", () => {
-    ctx.clearRect(0,0, 700, 700)
-  })
-  const ball = {
-    x: 15,
-    y: 15,
-    move: function() {
-      ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, true)
-      ctx.fillStyle = 'red'
-      ctx.fill()
-        },
+    ctx.clearRect(0, 0, 700, 700);
+  });
+
+  const canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+
+  class Ball {
+    constructor(ctx) {
+      (this.x = 10),
+        (this.y = 10),
+        (this.vx = 1),
+        (this.vy = 1),
+        (this.radius = 1),
+        (this.color = "blue"),
+        (this.ctx = ctx),
+        (this.movement = { up: false, down: false, left: false, right: false });
+    }
+
+    draw() {
+      this.ctx.beginPath();
+      this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      this.ctx.closePath();
+      this.ctx.fillStyle = this.color;
+      this.ctx.fill();
+    }
+
+    updatePosition() {
+      this.y = this.y;
+      if (this.movement.up) {
+        this.y -= 1;
+      }
+      if (this.movement.down) {
+        this.y += 1;
+      }
+      if (this.movement.right) {
+        this.x += 1;
+      }
+      if (this.movement.left) {
+        this.x -= 1;
+      }
+    }
   }
-  document.addEventListener("keydown", (e) => {
+
+  document.addEventListener("keydown", e => {
     switch (e.code) {
-      case "ArrowRight":
-        ball.x += 5;
-        break;
-      case "ArrowLeft":
-        ball.x -= 5
-        break;
       case "ArrowUp":
-        ball.y -= 5
+      case "w":
+        ball.movement.down = false;
+        ball.movement.up = true;
         break;
       case "ArrowDown":
-        ball.y += 10
+      case "s":
+        ball.movement.up = false;
+        ball.movement.down = true;
+        break;
+      case "ArrowRight":
+      case "d":
+        ball.movement.left = false;
+        ball.movement.right = true;
+        break;
+      case "ArrowLeft":
+      case "a":
+        ball.movement.right = false;
+        ball.movement.left = true;
         break;
       default:
-
     }
-  })
+  });
 
-  const canvas = document.getElementById('canvas');
-  let ctx = canvas.getContext('2d');
-  // let directions = {
-  //   left: false,
-  //   right: false,
-  //   up: false,
-  //   down: false
-  // }
+  let ball = new Ball(ctx);
 
-const draw_reset = () => {
-    const ctx = canvas.getContext("2d")
-    ctx.font = "50px Arial";
-    ctx.fillText("N",330,50);
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, 15, 0, Math.PI * 2, true)
-    ctx.moveTo(110, 110);
-    ctx.stroke();
-}
+  function step() {
+    // ctx.clearRect(0,0, canvas.width, canvas.height)
+    ball.draw();
+    if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+      ball.vy = -ball.vy;
+    }
+    if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+      ball.vx = -ball.vx;
+    }
+    ball.updatePosition()
+    requestAnimationFrame(step);
+  }
 
-function step() {
-
-  draw_reset()
-  ball.move()
-  requestAnimationFrame(step)
-}
-
-step()
-
-})
+  step();
+});
